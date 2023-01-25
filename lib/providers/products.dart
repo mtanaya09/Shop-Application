bb271b8e144a59834d605dc6afd00b694cd85644
mtanaya09 <mtanaya09@gmail.com>
2023-01-25 15:50:32 +0800

    Transform fetched data

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -5,39 +5,39 @@ import 'product.dart';
 
 class Products with ChangeNotifier {
   List<Product> _items = [
-    Product(
-      id: 'p1',
-      title: 'Red Shirt',
-      description: 'A red shirt - it is pretty red!',
-      price: 29.99,
-      imageUrl:
-          'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
-    ),
-    Product(
-      id: 'p2',
-      title: 'Trousers',
-      description: 'A nice pair of trousers.',
-      price: 59.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
-    ),
-    Product(
-      id: 'p3',
-      title: 'Brown Shoes',
-      description:
-          "Don't forget to wear your brown aesthetic shoes that are made for you!",
-      price: 19.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Pair_of_plain_toe_oxfords_from_Allen_Edmonds_shoe_company.jpg/640px-Pair_of_plain_toe_oxfords_from_Allen_Edmonds_shoe_company.jpg',
-    ),
-    Product(
-      id: 'p4',
-      title: 'A Pan',
-      description: 'Prepare any meal you want.',
-      price: 49.99,
-      imageUrl:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
-    ),
+    // Product(
+    //   id: 'p1',
+    //   title: 'Red Shirt',
+    //   description: 'A red shirt - it is pretty red!',
+    //   price: 29.99,
+    //   imageUrl:
+    //       'https://cdn.pixabay.com/photo/2016/10/02/22/17/red-t-shirt-1710578_1280.jpg',
+    // ),
+    // Product(
+    //   id: 'p2',
+    //   title: 'Trousers',
+    //   description: 'A nice pair of trousers.',
+    //   price: 59.99,
+    //   imageUrl:
+    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Trousers%2C_dress_%28AM_1960.022-8%29.jpg/512px-Trousers%2C_dress_%28AM_1960.022-8%29.jpg',
+    // ),
+    // Product(
+    //   id: 'p3',
+    //   title: 'Brown Shoes',
+    //   description:
+    //       "Don't forget to wear your brown aesthetic shoes that are made for you!",
+    //   price: 19.99,
+    //   imageUrl:
+    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ee/Pair_of_plain_toe_oxfords_from_Allen_Edmonds_shoe_company.jpg/640px-Pair_of_plain_toe_oxfords_from_Allen_Edmonds_shoe_company.jpg',
+    // ),
+    // Product(
+    //   id: 'p4',
+    //   title: 'A Pan',
+    //   description: 'Prepare any meal you want.',
+    //   price: 49.99,
+    //   imageUrl:
+    //       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+    // ),
   ];
 
   // var _showfavoritesOnly = false;
@@ -69,12 +69,27 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
+  //fetch data from the firebase database
   Future<void> fetchAndSetProducts() async {
     final url = Uri.parse(
         'https://flutter2023-e1efe-default-rtdb.firebaseio.com/products.json');
     try {
       final response = await http.get(url);
-      print(json.decode(response.body));
+      final extractedData = json.decode(response.body) as Map<String, dynamic>;
+      final List<Product> loadedProducts = [];
+      extractedData.forEach((prodId, prodData) {
+        loadedProducts.add(Product(
+          id: prodId,
+          title: prodData['title'],
+          description: prodData['description'],
+          price: prodData['price'],
+          isFavorite: prodData['isFavorite'],
+          imageUrl: prodData['imageUrl'],
+        ));
+      });
+      _items = loadedProducts;
+      notifyListeners();
+      // print(json.decode(response.body));
     } catch (error) {
       print(error);
       throw (error);

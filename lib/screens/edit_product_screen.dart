dc741d41e62f8a963f8cc98a7eb4f0435c56764c
mtanaya09 <mtanaya09@gmail.com>
2023-01-25 13:50:00 +0800

    Worked with handling errors

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -103,7 +103,24 @@ class _EditProductScreenState extends State<EditProductScreen> {
     } else {
       Provider.of<Products>(context, listen: false)
           .addProduct(_editedProduct)
-          .then((_) {
+          .catchError((error) {
+        return showDialog<Null>(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text('An error occured!'),
+            // content: Text(error.toString()),
+            content: Text('Something went wrong'),
+            actions: [
+              TextButton(
+                child: Text('Okay'),
+                onPressed: () {
+                  Navigator.of(ctx).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      }).then((_) {
         setState(() {
           _isLoading = false;
         });

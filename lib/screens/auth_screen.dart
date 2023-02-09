18747912b17d00f75f31c9d2d40ff42aa266c1e3
mtanaya09 <mtanaya09@gmail.com>
2023-02-09 11:19:25 +0800

    Fixed error display

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -259,7 +259,6 @@ class _AuthCardState extends State<AuthCard>
                     if (value.isEmpty || value.length < 5) {
                       return 'Password is too short!';
                     }
-                    return value;
                   },
                   onSaved: (value) {
                     _authData['password'] = value;
@@ -286,7 +285,6 @@ class _AuthCardState extends State<AuthCard>
                                 if (value != _passwordController.text) {
                                   return 'Passwords do not match!';
                                 }
-                                return value;
                               }
                             : null,
                       ),
